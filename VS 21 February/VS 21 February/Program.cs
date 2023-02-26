1f9edea74c58211ce0c742a1971ef57170e18a26
Anastasiya Zayats <nastyabonybelay@gmail.com>
2023-02-26 18:47:10 +0300

    add updated Programm.cs file

--- VS 21 February/VS 21 February/Program.cs
+++ VS 21 February/VS 21 February/Program.cs
@@ -5,10 +5,10 @@ class Hello
     static void Main(string[] args)
     {
         VS_21_February.Lesson3.Task0Way1();
-        //    VS_21_February.Lesson3.Task0Way2();
-        //    VS_21_February.Lesson3.Task1();
-        //    VS_21_February.Lesson3.Task2();
-        //    VS_21_February.Lesson3.Task3();
+        VS_21_February.Lesson3.Task0Way2();
+        VS_21_February.Lesson3.Task1();
+        VS_21_February.Lesson3.Task2();
+        VS_21_February.Lesson3.Task3();
     }
 
     public static void Lesson1_Task1()
