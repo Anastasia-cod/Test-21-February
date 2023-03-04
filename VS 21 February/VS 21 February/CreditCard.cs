59fc88468b2ea0830e5661b4de4f2111d7f2108c
Anastasiya Zayats <Nastyabonybelay@gmail.com>
2023-03-04 16:28:07 +0300

    “add ui improvement”

--- VS 21 February/VS 21 February/CreditCard.cs
+++ VS 21 February/VS 21 February/CreditCard.cs
@@ -3,39 +3,39 @@ namespace VS_21_February
 {
 	public class CreditCard
 	{
-		public string accountNumber;
-		public double currentSumm;
+        public string accountNumber;
+        public double currentSumm;
 
-		public CreditCard()
-		{
+        public CreditCard()
+        {
 
-		}
+        }
 
-		public double AccrualMoney(double summOfMoney)
-		{
-			currentSumm += summOfMoney;
-			return currentSumm;
-		}
+        public double AccrualMoney(double summOfMoney)
+        {
+            currentSumm += summOfMoney;
+            return currentSumm;
+        }
 
-		public double WithdrawalMoney(double summOfMoney)
-		{
+        public double WithdrawalMoney(double summOfMoney)
+        {
             if (summOfMoney > currentSumm)
-			{
+            {
                 Console.WriteLine($"Error! Not enough money. Available summ of money on the card is {currentSumm}");
             }
             else
-			{
+            {
                 currentSumm -= summOfMoney;
             }
 
             return currentSumm;
         }
 
-		public void CurrentStatus ()
-		{
-			Console.WriteLine($"The current summ is {currentSumm}");
-		}
-
+        public void CurrentStatus()
+        {
+            Console.WriteLine($"The current summ is {currentSumm}");
+        }
     }
+
 }
 
