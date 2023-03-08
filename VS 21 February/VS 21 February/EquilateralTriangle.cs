ed44f575b811381d9fb6e2e2cac248dd05397cd4
Anastasiya Zayats <Nastyabonybelay@gmail.com>
2023-03-08 13:01:26 +0300

    “add fix style’

--- VS 21 February/VS 21 February/EquilateralTriangle.cs
+++ VS 21 February/VS 21 February/EquilateralTriangle.cs
@@ -3,7 +3,6 @@ namespace VS_21_February
 {
 	public class EquilateralTriangle : Triangles
 	{
-
 		public EquilateralTriangle(double firstSide, double secondSide, double thirdSide) :base(firstSide, secondSide, thirdSide)
 		{
 		}
