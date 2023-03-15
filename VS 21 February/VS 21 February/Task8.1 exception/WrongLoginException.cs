fbc8ba36bcc11824e71c8caa1f77c72edaeebc2f
Anastasiya Zayats <Nastyabonybelay@gmail.com>
2023-03-15 11:18:36 +0300

    add fix style

--- VS 21 February/VS 21 February/Task8.1 exception/WrongLoginException.cs
+++ VS 21 February/VS 21 February/Task8.1 exception/WrongLoginException.cs
@@ -3,11 +3,9 @@ namespace VS_21_February.Task8.exception
 {
 	public class WrongLoginException : Exception
 	{
-		public string Detail { get; set; }
-
-        public WrongLoginException(string message) : base(message)
-        {
-        }
-    }
+		public WrongLoginException(string message) : base(message)
+		{
+		}
+	}
 }
 
