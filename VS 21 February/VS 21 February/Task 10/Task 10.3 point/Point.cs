6042fda44d5f2bc11cf3e1526aaa40e37760e616
Anastasiya Zayats <Nastyabonybelay@gmail.com>
2023-03-28 08:46:13 +0300

    add fix task points

--- VS 21 February/VS 21 February/Task 10/Task 10.3 point/Point.cs
+++ VS 21 February/VS 21 February/Task 10/Task 10.3 point/Point.cs
@@ -1,53 +1,45 @@
 ﻿using System;
 namespace VS_21_February.Task10.point
 {
-	public class Point <T> where T : struct
-	{
-		public T x;
-		public T y;
+    public class Point <T> where T : struct
+    {
+        public T x;
+        public T y;
 
-		public T X
-		{
-			get { return x; }
-			set { x = value; }
-		}
+        public T X {get; set;}
 
-		public T Y
-		{
-			get { return y; }
-			set { y = value; }
-		}
+        public T Y {get; set;}
 
-		public Point(T x, T y)
-		{
-			this.x = x;
-			this.y = y;
-		}
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
-		/// <summary>
-		/// Get info about Point
-		/// </summary>
-		public void GetInfo()
-		{
-			Console.WriteLine($"\nСoordinate x: {x}. Coordinate y: {y}");
-		}
+        /// <summary>
+        /// Get info about Point
+        /// </summary>
+        public void GetInfo()
+        {
+            Console.WriteLine($"\nСoordinate x: {x}. Coordinate y: {y}");
+        }
 
-		/// <summary>
-		/// Get distance beetween two coordinate Point_1(x; y) and Point_2(x; y)
-		/// </summary>
-		/// <param name="first"></param>
-		/// <param name="second"></param>
-		public void GetDistanceBetweenTwoPoints(Point<T> first, Point<T> second)
-		{
-			double x_First = Convert.ToDouble(first.x);
-			double y_First = Convert.ToDouble(first.y);
-			double x_Second = Convert.ToDouble(second.x);
-			double y_Second = Convert.ToDouble(second.y);
+        /// <summary>
+        /// Get distance beetween two coordinate Point_1(x; y) and Point_2(x; y)
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        public void GetDistanceBetweenTwoPoints(Point<T> first, Point<T> second)
+        {
+            double x_First = Convert.ToDouble(first.x);
+            double y_First = Convert.ToDouble(first.y);
+            double x_Second = Convert.ToDouble(second.x);
+            double y_Second = Convert.ToDouble(second.y);
 
-			var distanceBetweenTwoPoint = Math.Sqrt(Math.Pow((x_First - x_Second), 2) + Math.Pow((y_First - y_Second), 2));
+            var distanceBetweenTwoPoint = Math.Sqrt(Math.Pow((x_First - x_Second), 2) + Math.Pow((y_First - y_Second), 2));
 
-			Console.WriteLine($"Distance beetween two points: {distanceBetweenTwoPoint}");
-		}
-	}
+            Console.WriteLine($"Distance beetween two points: {distanceBetweenTwoPoint}");
+        }
+    }
 }
 
