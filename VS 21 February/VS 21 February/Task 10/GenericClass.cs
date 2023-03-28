0ba69297ffac70381525f3282251e4bf2eef94cf
Anastasiya Zayats <Nastyabonybelay@gmail.com>
2023-03-28 08:49:30 +0300

    fix code style

--- VS 21 February/VS 21 February/Task 10/GenericClass.cs
+++ VS 21 February/VS 21 February/Task 10/GenericClass.cs
@@ -1,101 +1,101 @@
 ﻿using System;
 namespace VS_21_February.Task10
 {
-	public class GenericClass<T>
-	{
-		public T[] array;
+    public class GenericClass<T>
+    {
+        public T[] array;
 
-		public GenericClass()
-		{
-			array = Array.Empty<T>();
-		}
+        public GenericClass()
+        {
+            array = Array.Empty<T>();
+        }
 
-		/// <summary>
-		/// Add item to Array
-		/// </summary>
-		/// <param name="item"></param>
-		public void AddItemToArray(T item)
-		{
-			T[] newArray = new T[array.Length + 1];
+        /// <summary>
+        /// Add item to Array
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddItemToArray(T item)
+        {
+            T[] newArray = new T[array.Length + 1];
 
-			for(int i =0; i < array.Length; i++)
-			{
-				newArray[i] = array[i];
-			}
+            for(int i =0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
 
-			newArray[array.Length] = item;
-			array = newArray;
-		}
+            newArray[array.Length] = item;
+            array = newArray;
+        }
 
-		/// <summary>
-		/// Remove item from Array
-		/// </summary>
-		/// <param name="item"></param>
-		public void RemoveItemFromArray(T item)
-		{
-			int findIndex = -1;
-			for (int i = 0; i < array.Length; i++)
-			{
-				if (array[i].Equals(item))
-				{
-					findIndex = i;
-					break;
-				}
-			}
+        /// <summary>
+        /// Remove item from Array
+        /// </summary>
+        /// <param name="item"></param>
+        public void RemoveItemFromArray(T item)
+        {
+            int findIndex = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    findIndex = i;
+                    break;
+                }
+            }
 
-			if (findIndex > -1)
-			{
-				T[] newArray = new T[array.Length - 1];
-				for (int i = 0, j = 0; i < array.Length; i++)
-				{
-					if (i == findIndex)
-						continue;
+            if (findIndex > -1)
+            {
+                T[] newArray = new T[array.Length - 1];
+                for (int i = 0, j = 0; i < array.Length; i++)
+                {
+                    if (i == findIndex)
+                        continue;
 
-					newArray[j] = array[i];
-					j++;
-				}
+                    newArray[j] = array[i];
+                    j++;
+                }
 
-				array = newArray;
-			}
-		}
+                array = newArray;
+            }
+        }
 
-		/// <summary>
-		/// Find item in Array by Index
-		/// </summary>
-		/// <param name="index"></param>
-		public void GetItemByIndex(int index)
-		{
-			try
-			{
-				T itemInArray = array[index];
+        /// <summary>
+        /// Find item in Array by Index
+        /// </summary>
+        /// <param name="index"></param>
+        public void GetItemByIndex(int index)
+        {
+            try
+            {
+                T itemInArray = array[index];
 
-				if (index > -1 && index < array.Length)
-				{
-					Console.WriteLine(itemInArray);
-				}
-				else
-				{
-					throw new IndexOutOfRangeException();
-				}
-			}
-			catch (IndexOutOfRangeException ex)
-			{
-				Console.WriteLine($"\n{ex.Message}\nThe array doesn't consist such index. Max index in the array is {array.Length -1}");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"\n{ex.Message}");
-			}
-		}
+                if (index > -1 && index < array.Length)
+                {
+                    Console.WriteLine(itemInArray);
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"\n{ex.Message}\nThe array doesn't consist such index. Max index in the array is {array.Length -1}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\n{ex.Message}");
+            }
+        }
 
-		/// <summary>
-		/// Return Array Length
-		/// </summary>
-		/// <returns></returns>
-		public int ArrayLength()
-		{
-			return array.Length;
-		}
-	}
+        /// <summary>
+        /// Return Array Length
+        /// </summary>
+        /// <returns></returns>
+        public int ArrayLength()
+        {
+            return array.Length;
+        }
+    }
 }
 
--- VS 21 February/VS 21 February/Task 10/Task 10.2 car and engine/PetrolEngine.cs
+++ VS 21 February/VS 21 February/Task 10/Task 10.2 car and engine/PetrolEngine.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 namespace VS_21_February.Task10.Task10.carandengine
 {
-	public class PetrolEngine : Engine
-	{
-		public PetrolEngine(string type = "Petrol") : base(type)
-		{
-		}
+    public class PetrolEngine : Engine
+    {
+        public PetrolEngine(string type = "Petrol") : base(type)
+        {
+        }
 
-		public override void GetInfo()
-		{
-			Console.WriteLine($"The engine is {type}");
-		}
-	}
+        public override void GetInfo()
+        {
+            Console.WriteLine($"The engine is {type}");
+        }
+    }
 }
 
